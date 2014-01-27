60f72c6f59c439e89866f044933dbd42fe2b5122
Satoshi Hafuka <hafuka_satoshi_gn@cyberagent.co.jp>
2014-01-27 16:59:29 +0900

    テストデータ UFT-8

---
+++ src/main/resources/data/coupon_data_2.sql
@@ -0,0 +1,115 @@
+insert into M_LOGIN_BONUS values(1,100,1);
+insert into M_CONFIG values('LOGIN_BONUS_TIME', '00:00', null,null);
+
+insert into M_AREA values(1,'北海道');
+insert into M_AREA values(2,'青森県');
+insert into M_AREA values(3,'秋田県');
+insert into M_AREA values(4,'岩手県');
+insert into M_AREA values(5,'山形県');
+insert into M_AREA values(6,'宮城県');
+insert into M_AREA values(7,'福島県');
+insert into M_AREA values(8,'茨城県');
+insert into M_AREA values(9,'栃木県');
+insert into M_AREA values(10,'群馬県');
+insert into M_AREA values(11,'埼玉県');
+insert into M_AREA values(12,'神奈川県');
+insert into M_AREA values(13,'千葉県');
+insert into M_AREA values(14,'東京都');
+insert into M_AREA values(15,'山梨県');
+insert into M_AREA values(16,'長野県');
+insert into M_AREA values(17,'新潟県');
+insert into M_AREA values(18,'富山県');
+insert into M_AREA values(19,'石川県');
+insert into M_AREA values(20,'福井県');
+insert into M_AREA values(21,'岐阜県');
+insert into M_AREA values(22,'静岡県');
+insert into M_AREA values(23,'愛知県');
+insert into M_AREA values(24,'三重県');
+insert into M_AREA values(25,'滋賀県');
+insert into M_AREA values(26,'京都府');
+insert into M_AREA values(27,'大阪府');
+insert into M_AREA values(28,'兵庫県');
+insert into M_AREA values(29,'奈良県');
+insert into M_AREA values(30,'和歌山県');
+insert into M_AREA values(31,'鳥取県');
+insert into M_AREA values(32,'島根県');
+insert into M_AREA values(33,'岡山県');
+insert into M_AREA values(34,'広島県');
+insert into M_AREA values(35,'山口県');
+insert into M_AREA values(36,'徳島県');
+insert into M_AREA values(37,'香川県');
+insert into M_AREA values(38,'愛媛県');
+insert into M_AREA values(39,'高知県');
+insert into M_AREA values(40,'福岡県');
+insert into M_AREA values(41,'佐賀県');
+insert into M_AREA values(42,'長崎県');
+insert into M_AREA values(43,'熊本県');
+insert into M_AREA values(44,'大分県');
+insert into M_AREA values(45,'宮崎県');
+insert into M_AREA values(46,'鹿児島県');
+insert into M_AREA values(47,'沖縄県');
+
+
+insert into M_AREA_DETAIL values(14,1,'銀座・新橋・有楽町');
+insert into M_AREA_DETAIL values(14,2,'東京・日本橋');
+insert into M_AREA_DETAIL values(14,3,'渋谷・恵比寿・代官山');
+insert into M_AREA_DETAIL values(14,4,'新宿・代々木・大久保');
+insert into M_AREA_DETAIL values(14,5,'池袋～高田馬場・早稲田');
+insert into M_AREA_DETAIL values(14,6,'原宿・表参道・青山');
+insert into M_AREA_DETAIL values(14,7,'六本木・麻布・広尾');
+insert into M_AREA_DETAIL values(14,8,'赤坂・永田町・溜池');
+insert into M_AREA_DETAIL values(14,9,'四ツ谷・市ヶ谷・飯田橋');
+insert into M_AREA_DETAIL values(14,10,'秋葉原・神田・水道橋');
+insert into M_AREA_DETAIL values(14,11,'上野・浅草・日暮里');
+insert into M_AREA_DETAIL values(14,12,'両国・錦糸町・小岩');
+insert into M_AREA_DETAIL values(14,13,'築地・湾岸・お台場');
+insert into M_AREA_DETAIL values(14,14,'浜松町・田町・品川');
+insert into M_AREA_DETAIL values(14,15,'大井・蒲田');
+insert into M_AREA_DETAIL values(14,16,'目黒・白金・五反田');
+insert into M_AREA_DETAIL values(14,17,'東急沿線');
+insert into M_AREA_DETAIL values(14,18,'京王・小田急沿線');
+insert into M_AREA_DETAIL values(14,19,'中野～西荻窪');
+insert into M_AREA_DETAIL values(14,20,'吉祥寺・三鷹・武蔵境');
+insert into M_AREA_DETAIL values(14,21,'西武沿線');
+insert into M_AREA_DETAIL values(14,22,'板橋・東武沿線');
+insert into M_AREA_DETAIL values(14,23,'大塚・巣鴨・駒込・赤羽');
+insert into M_AREA_DETAIL values(14,24,'千住・綾瀬・葛飾');
+insert into M_AREA_DETAIL values(14,25,'小金井・国分寺・国立');
+insert into M_AREA_DETAIL values(14,26,'調布・府中・狛江');
+insert into M_AREA_DETAIL values(14,27,'町田・稲城・多摩');
+insert into M_AREA_DETAIL values(14,28,'西東京市周辺');
+insert into M_AREA_DETAIL values(14,29,'立川市・八王子市周辺');
+insert into M_AREA_DETAIL values(14,30,'福生・青梅周辺');
+insert into M_AREA_DETAIL values(14,31,'伊豆諸島・小笠原');
+
+
+insert into M_SHOP values(1,1,'日比谷bar','日比谷bar説明',null,null,null,14,1,'渋谷',0);
+insert into M_SHOP values(2,1,'とりしょう','とりしょう説明',null,null,null,14,1,'渋谷',0);
+insert into M_SHOP values(3,1,'鳥竹','鳥竹説明',null,null,null,14,1,'渋谷',0);
+insert into M_SHOP values(4,1,'cona','cona説明',null,null,null,14,1,'渋谷',0);
+
+insert into M_BUSINESS values(1,'飲食店');
+insert into M_BUSINESS values(2,'美容院');
+insert into M_BUSINESS values(3,'キャバクラ');
+insert into M_BUSINESS values(4,'アパレル');
+
+
+insert into M_SHOP_COUPON values(1,1,1,'10%OFF', '10%OFF', 40, 3);
+insert into M_SHOP_COUPON values(1,1,2,'20%OFF', '20%OFF', 30, 3);
+insert into M_SHOP_COUPON values(1,1,3,'30%OFF', '30%OFF', 20, 3);
+insert into M_SHOP_COUPON values(1,1,4,'50%OFF', '50%OFF', 10, 3);
+
+insert into M_SHOP_COUPON values(2,1,1,'10%OFF', '10%OFF', 40, 3);
+insert into M_SHOP_COUPON values(2,1,2,'20%OFF', '20%OFF', 30, 3);
+insert into M_SHOP_COUPON values(2,1,3,'30%OFF', '30%OFF', 20, 3);
+insert into M_SHOP_COUPON values(2,1,4,'50%OFF', '50%OFF', 10, 3);
+
+insert into M_SHOP_COUPON values(3,1,1,'10%OFF', '10%OFF', 40, 3);
+insert into M_SHOP_COUPON values(3,1,2,'20%OFF', '20%OFF', 30, 3);
+insert into M_SHOP_COUPON values(3,1,3,'30%OFF', '30%OFF', 20, 3);
+insert into M_SHOP_COUPON values(3,1,4,'50%OFF', '50%OFF', 10, 3);
+
+insert into M_SHOP_COUPON values(4,1,1,'10%OFF', '10%OFF', 40, 3);
+insert into M_SHOP_COUPON values(4,1,2,'20%OFF', '20%OFF', 30, 3);
+insert into M_SHOP_COUPON values(4,1,3,'30%OFF', '30%OFF', 20, 3);
+insert into M_SHOP_COUPON values(4,1,4,'50%OFF', '50%OFF', 10, 3);

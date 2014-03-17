85e63e69a76ecea8984ad678de37a1dac0e52e95
Satoshi Hafuka <hafuka_satoshi_gn@cyberagent.co.jp>
2014-03-17 13:07:40 +0900

    未入力対応

--- src/main/java/coupon/action/ShopSearchAction.java
+++ src/main/java/coupon/action/ShopSearchAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.StringUtils;
 import org.seasar.struts.annotation.Execute;
 
 import coupon.bean.ShopBean;
@@ -14,22 +15,25 @@ import coupon.service.ShopService;
  * ショップ検索
  */
 public class ShopSearchAction extends BaseAction {
-	
+
 	@Resource
 	protected ShopService shopService;
-	
+
 	public String searchValue;
-	
+
 	public List<ShopBean> shopList;
-	
+
 	@Execute(validator=false)
 	public String index() {
 		shopList = shopService.getShopBaens(null, null, null);
 		return "shop_search.ftl";
 	}
-	
+
 	@Execute(validator=false)
 	public String search() throws IOException {
+		if (StringUtils.isEmpty(StringUtils.trimToEmpty(searchValue))) {
+			searchValue = null;
+		}
 		List<ShopBean> shopList = shopService.getShopBeans(searchValue);
 		super.setJsonData(shopList);
 		return null;
